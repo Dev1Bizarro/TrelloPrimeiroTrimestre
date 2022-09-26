8710a6ad6a1549aeb28867fdbc47bc076bd44607
Gustavo Bizarro <dev.biza@gmail.com>
2022-09-26 16:48:16 -0300

    Exercício 01 trello

---
+++ src/ex01.java
@@ -0,0 +1,13 @@
+public class ex01 {
+    // Implemente um código copiando o array abaixo, e depois imprima no console o valor dos seguintes elementos
+    //a) Imprima o valor do primeiro elemento;
+    //b) Imprima o valor do último elemento;
+    //c) Imprima o valor do terceiro elemento;
+    public static void main(String[] args) {
+        int[] arr = { 56, 57, 58, 67, 99, 12, 11, 88 };
+
+        System.out.println("Esse é o priemrio elemento do array " + arr[0] );
+        System.out.println("Esse é o ultimo elemento do array " + arr[7] );
+        System.out.println("Esse é o terceiro elemento do array " + arr[2] );
+    }
+}
